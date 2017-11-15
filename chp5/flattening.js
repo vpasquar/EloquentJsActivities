f98c98923240c1a70f41cf7e05ac15a554943c1f
vin <vrp908@gmail.com>
2017-11-15 14:02:49 -0500

    flattening

--- chp5/flattening.js
+++ chp5/flattening.js
@@ -1,1 +1,7 @@
-//
+//flatten an array of arrays into a single array using reduce and concat.
+
+const arrays = [[1,2,3],[4,5],[6]];
+
+console.log(arrays.reduce(function(a,b) {
+	return a.concat(b);
+}));
