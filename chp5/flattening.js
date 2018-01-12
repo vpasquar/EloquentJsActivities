40ab55edf70e26bc48418123bc7fabe49472c83f
vin <vrp908@gmail.com>
2018-01-12 12:14:32 -0500

    bubsort

--- chp5/flattening.js
+++ chp5/flattening.js
@@ -3,6 +3,7 @@
 const arrays = [[1,2,3],[4,5],[6]];
 
 //reduce function goes through each element in arrays and concatenates them in order.
-console.log(arrays.reduce(function(a,b) {
-	return a.concat(b);
-}));
+
+
+
+console.log(arrays.reduce = (a,b) => a.concat(b));
---
+++ udemy/chp2/bubsort.js
@@ -0,0 +1,38 @@
+const arr = [3,10,4,13,1]
+ 
+console.log(arr); 
+
+
+bubSort1 = arr => {
+	for (let x = 0; x < arr.length - 2; x++) {
+	  
+
+	    for (let j = arr.length - 1; j > 0; j--) {
+
+	   	    if (arr[j] < arr[j-1]) {
+	   	    	let tmp = arr[j]
+	   	    	arr[j] = arr[j-1]
+	   	    	arr[j-1] = tmp
+	   	    }
+	    }
+	}
+	return arr;
+}
+
+bubSort2 = arr => {
+	for (let x = 0; x < arr.length-1; x++) {
+		for(let j = 0; j < arr.length - 1 - x; j++){
+			if (arr[j] > arr[j+1]) {
+				let tmp = arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = tmp
+			}
+		}
+	}
+	return arr;
+}
+console.log(bubSort1(arr));
+
+
+const arr2 = [14,12,7,3,10]
+console.log(bubSort2(arr2))
