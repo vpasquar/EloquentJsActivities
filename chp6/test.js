a9511f991c13ade81e7e0bdfde3ac658f268d3bf
vin <vrp908@gmail.com>
2018-02-01 13:57:41 -0500

    matrix2

--- chp6/test.js
+++ chp6/test.js
@@ -40,6 +40,10 @@ class MatrixIterator {
   }
 }
 
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+};
+
 let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`);
 for (let {x, y, value} of matrix) {
   console.log(x, y, value);
